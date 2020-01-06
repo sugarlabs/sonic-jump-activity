fefe3923c4e7d1b8d756613d5ec3780a6fdad68b
Srevin Saju <srevin03@gmail.com>
2020-01-06 16:07:05 +0300

    Port to Python3 (2to3)

--- scorescreen.py
+++ scorescreen.py
@@ -83,9 +83,9 @@ class scorescreen:
 
         try:
             pygame.mixer.init()
-        except Exception, err:
+        except Exception as err:
             sound = False
-            print 'error with sound', err
+            print('error with sound', err)
 
         black = (0, 0, 0)
         white = (255, 255, 255)
--- welcomescreen.py
+++ welcomescreen.py
@@ -80,9 +80,9 @@ class welcomescreen:
 
         try:
             pygame.mixer.init()
-        except Exception, err:
+        except Exception as err:
             sound = False
-            print 'error with sound', err
+            print('error with sound', err)
 
         black = (0, 0, 0)
         white = (255, 255, 255)
